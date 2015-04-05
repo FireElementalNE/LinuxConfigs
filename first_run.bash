72c6ae74b52c2c450747be7fb8ac8b04f6bc53ec
Lindsey <FireElementalNE@users.noreply.github.com>
2015-04-05 11:14:11 -0400

    Update first_run.bash

--- first_run.bash
+++ first_run.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y guake gcc g++ python emacs emacs-goodies-el perl make units git subverion git-cola fish python-pip
+sudo apt-get install -y guake gcc g++ python emacs emacs-goodies-el perl make units git subverion git-cola fish python-pip screen
