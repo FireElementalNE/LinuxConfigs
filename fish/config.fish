b1c14061a2bcd41a04b0ccba85b29e31e49b028c
Lindsey <FireElementalNE@users.noreply.github.com>
2015-04-05 15:29:14 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,44 @@
-echo "hello!"
+#
+# Init file for fish
+#
+
+#
+# Some things should only be done for login terminals
+#
+
+if status --is-login
+
+	#
+	# Set some value for LANG if nothing was set before, and this is a
+	# login shell.
+	#
+
+	if not set -q LANG >/dev/null
+		set -gx LANG en_US.UTF-8
+	end
+
+	# Check for i18n information in
+	# /etc/sysconfig/i18n
+
+	if test -f /etc/sysconfig/i18n
+		eval (cat /etc/sysconfig/i18n |sed -ne 's/^\([a-zA-Z]*\)=\(.*\)$/set -gx \1 \2;/p')
+	end
+
+	#
+	# Put linux consoles in unicode mode.
+	#
+
+	if test "$TERM" = linux
+		if expr "$LANG" : ".*\.[Uu][Tt][Ff].*" >/dev/null
+			if which unicode_start >/dev/null
+				unicode_start
+			end
+		end
+	end
+end
+set base_color normal
+set offset_color red
+
 function fish_prompt
-            set_color green
-            date "+%H:%M >> "
-        end
+    printf '%s%s %s%s%s >> ' (set_color $base_color) (prompt_pwd) (set_color $offset_color) (date "+%H:%M:%S") (set_color $base_color)
+end⏎
