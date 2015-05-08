b2e7b23a571350d2b40e8e13f7f83d891ecc24ce
FireElementalNE <FireElemental.NE@gmail.com>
2015-05-08 15:07:00 -0400

    changes

--- first_run.bash
+++ first_run.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y guake gcc g++ python emacs emacs-goodies-el perl make units git subverion git-cola fish python-pip screen
+sudo apt-get install -y guake gcc g++ python emacs emacs-goodies-el perl make units git git-cola fish python-pip screen
