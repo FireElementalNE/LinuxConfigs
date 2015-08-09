339628ec6bb81817f99aa3042b3c65faee9c7f54
FireElementalNE <FireElemental.NE@gmail.com>
2015-08-09 12:55:38 -0400

    fixed

--- arch/arch.bash
+++ arch/arch.bash
@@ -1,11 +1,7 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-pacman -Syu
-pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake gcc  python2 emacs  perl  make  units  fish  python-pip screen 
-cp ../emacs.el ~/.emacs
+sudo pacman -Syu
+# sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake gcc  python2 emacs  perl  make  units  fish  python-pip screen 
+cp ../emacs.el $HOME/.emacs
 wget https://raw.githubusercontent.com/juba/color-theme-tangotango/master/color-theme-tangotango.el
-mv color-theme-tangotango.el ~/emacs.d/
-cp ../config.fish /etc/fish/
+mv color-theme-tangotango.el $HOME/.emacs.d/
+sudo cp ../config.fish /etc/fish/
