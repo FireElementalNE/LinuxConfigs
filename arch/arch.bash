9d73dca2cd9361f944128b5ff4eb859c004464dc
FireElementalNE <FireElemental.NE@gmail.com>
2015-11-05 09:44:33 -0500

    fixed

--- arch/arch.bash
+++ arch/arch.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake gcc  python2 emacs  perl  make  units  fish  python-pip screen viewnior
+sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake python2 emacs perl units fish python-pip screen viewnior
 cp ../emacs.el $HOME/.emacs
-wget https://raw.githubusercontent.com/juba/color-theme-tangotango/master/color-theme-tangotango.el
-mv color-theme-tangotango.el $HOME/.emacs.d/
+wget https://raw.githubusercontent.com/juba/color-theme-tangotango/master/tangotango-theme.el
+mv tangotango-theme.el $HOME/.emacs.d/
 sudo cp ../config.fish /etc/fish/
