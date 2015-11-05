1c07ef18dc3b400853eda7bf004e4ed560d680cc
FireElementalNE <FireElemental.NE@gmail.com>
2015-11-05 10:26:48 -0500

    added vim to arch config

--- arch/arch.bash
+++ arch/arch.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake python2 emacs perl units fish python-pip screen viewnior
+sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol gnome-terminal flashplugin vlc chromium unzip unrar p7zip gedit gnome-system-monitor a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins git guake python2 emacs perl units fish python-pip screen viewnior vim
 cp ../emacs.el $HOME/.emacs
 wget https://raw.githubusercontent.com/juba/color-theme-tangotango/master/tangotango-theme.el
 mv tangotango-theme.el $HOME/.emacs.d/
