5b9a299d1de0c475c48db5f07cf7dcea9fddfe2f
FireElementalNE <FireElemental.NE@gmail.com>
2016-02-23 17:18:03 -0500

    changed format again

--- config.fish
+++ config.fish
@@ -48,7 +48,7 @@ function fish_prompt
 	    set ucolor FF6666
 	    set pchar "# "
 	 end
-	 echo $rc (set_color $dcolor)(date "+%r")(set_color normal):(set_color $ucolor)(whoami)@(hostname)(set_color normal):(set_color $pcolor)" "(prompt_pwd) $pchar
+	 echo $rc (set_color $dcolor)(date "+%I:%M%p")(set_color normal):(set_color $ucolor)(whoami)@(hostname)(set_color normal):(set_color $pcolor)" "(prompt_pwd) $pchar
 end
 
 alias ll='ls -alF'
